9da913eec5f49b9d52a5f33ece4be2691fec1ea4
Byeong Gyu Kim <qudrb6989@postech.ac.kr>
2022-06-14 16:08:22 +0900

    2022.6.14 16:10

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ def pib_list(n: int) -> list:
     return result
 
 @app.command()
-def pib(name: str, n: int, agg: bool = False):
+def pib(name: str, n: int = typer.Argument(..., min = 1), agg: bool = False):
     piblist = pib_list(n)
+    piblist_str = [str(a) for a in piblist]
     if agg:
         for line in range(n):
-            for i in range(1, line+2):
-                print(piblist[i], end=' ')
-            print(" ")
+            piblist_ans = ' '.join(piblist_str[1:line+2])
+            print(piblist_ans)
+
     else:
-        for line in range(1, n+1):
-            print(piblist[line], end=' ')
-        print("")
+        piblist_ans = ' '.join(piblist_str[1:n+1])
+        print(piblist_ans)
 
 if __name__ == "__main__":
     app()
